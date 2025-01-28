92b07fe7f143224bf3f5d9f52cf612a073538e18
Gustavo-Henrique444 <idontlikeflatytgirls@gmail.com>
2025-01-28 20:57:04 -0300

    Typo ⚙

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", '@typescript-eslint/no-explicit-any'),
 ];
 
 export default eslintConfig;
